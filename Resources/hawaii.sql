272e8b0bfe546fe909bd516a2da6ed265ec531a9
Cnw695 <56791995+Cnw695@users.noreply.github.com>
2020-02-23 21:33:14 -0500

    rerun

--- Resources/hawaii.sql
+++ Resources/hawaii.sql
@@ -1,17 +1,23 @@
 DROP TABLE IF EXISTS station;
 DROP TABLE IF EXISTS measurements;
 
-CREATE TABLE station(
-	station VARCHAR(255) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	latitude FLOAT NOT NULL,
-	longitude FLOAT NOT NULL,
-	elevation FLOAT NOT NULL
+CREATE TABLE station (
+    station VARCHAR(255)   NOT NULL,
+    name VARCHAR(255)   NOT NULL,
+    latitude FLOAT   NOT NULL,
+    longitude FLOAT  NOT NULL,
+    elevation FLOAT  NOT NULL,
+    CONSTRAINT "pk_station" PRIMARY KEY (
+        "station"
+     )
 );
 
-CREATE TABLE measurements(
-	station VARCHAR(255) NOT NULL,
-	date VARCHAR(255) NOT NULL,
-	prcp FLOAT,
-	tobs INT
+CREATE TABLE measurements (
+    station VARCHAR(255)   NOT NULL,
+    date date   NOT NULL,
+    prcp FLOAT,
+    tobs INT   
 );
+
+ALTER TABLE measurements ADD CONSTRAINT fk_measurements_station FOREIGN KEY(station)
+REFERENCES station (station);
